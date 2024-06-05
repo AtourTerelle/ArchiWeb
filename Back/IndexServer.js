12151e8f97c7890552cb2f5908ca7d97945b4475
Doryan <122481253+AtourTerelle@users.noreply.github.com>
2024-06-05 22:46:00 +0200

    Le json le json

--- Back/IndexServer.js
+++ Back/IndexServer.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.listen(5000);
+console.log("le test la");
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
+
+app.listen(5000, ()=> {
+    console.log("le port batard");
+});
