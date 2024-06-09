e7794ab2dc5f20f6550476aa3fab9719128bdc3e
Doryan <122481253+AtourTerelle@users.noreply.github.com>
2024-06-09 16:49:48 +0200

    pour toi milo

--- Back/IndexServer.js
+++ Back/IndexServer.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 
+var mongoose = require('mongoose')
+
 console.log("le test la");
+
+mongoose.connect("mongodb://127.0.0.1:27017/ArchiWeb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-  });
-  
-
+  }); 
   
 app.listen(5000, ()=> {
     console.log("le port batard");
 });
+
