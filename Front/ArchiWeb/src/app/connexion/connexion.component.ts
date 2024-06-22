6f33ce04b22d356aed75d375fb91e31e08f84c30
Doryan <122481253+AtourTerelle@users.noreply.github.com>
2024-06-22 22:40:08 +0200

    Token dans Local storage
    Mais j'ai du tricher et enlever le catch de l'erreur

--- Front/ArchiWeb/src/app/connexion/connexion.component.ts
+++ Front/ArchiWeb/src/app/connexion/connexion.component.ts
@@ -25,20 +25,39 @@ export class ConnexionComponent {
   }
 
   onSubmit(): void {
+
     if (this.connexionForm.valid) {
       const userData = {
         pseudo_u: this.connexionForm.value.pseudo,
         mdp_u: this.connexionForm.value.mdp
       };
+
       const headers = new HttpHeaders()
-      this.http.post('http://localhost:5000/connexion', userData, {headers})
-        .subscribe(
-          response => {
+      
+      this.http.post<any>('http://localhost:5000/connexion', userData, {headers})
+        .subscribe({
+          next: (response) => {
+            if(response.token){
+              localStorage.setItem('authToken', response.token);
+              console.log('connexion reussi', response);
+            }
+            else{
+              console.error('Erreur lors de la connexion');
+            }
+          },
+        }
+          /*response => {
+            if(response.token)
+              response.json()
+            console.log(response);
+            console.log(response.token);
+            //localStorage.setItem('authToken', response.token);
+
             console.log('connexion reussi', response);
           },
           error => {
             console.error('Erreur lors de la connexion', error);
-          }
+          }*/
         );
     }
   }
