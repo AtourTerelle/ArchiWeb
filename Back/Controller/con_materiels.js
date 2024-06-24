1191d6756d22e0f9b7a9d47dc1c36515f7cfbad7
Doryan <122481253+AtourTerelle@users.noreply.github.com>
2024-06-24 16:14:18 +0200

    La liberté + la deco qui enleve pas dans le local storage

--- Back/Controller/con_materiels.js
+++ Back/Controller/con_materiels.js
@@ -2,7 +2,7 @@ const materiels = require("../Model/materiels");
 
 exports.materielsDispo = async (req, res) => {
     try {
-        const materieldispo = await materiels.find({ Reserve_par: null });
+        const materieldispo = await materiels.find({ reserve_par: "Libre" });
         res.status(200).json(materieldispo);
     } catch (error) {
         res.status(500).json({ message: "Erreur du serveur", error: error.message });
@@ -20,9 +20,9 @@ exports.allmateriels = async (req, res) => {
 
 exports.materielsDispoByType = async (req, res) => {
     try {
-        const { type } = req.body;
+        const { type_m } = req.body;
 
-        const materielsdispo = await materiels.find({ type_m: type, Reserve_par: null });
+        const materielsdispo = await materiels.find({ type_m: type_m, reserve_par: "Libre" });
         console.log(materielsdispo)
 
 
@@ -57,8 +57,8 @@ exports.AddMateriels = async (req, res) => {
         const newMateriel = new materiels({
             nom_m,
             type_m,
-            reserve_par: "",
-            salle
+            reserve_par: "Libre",
+            salle: "Reserve"
         });
 
 
--- Front/ArchiWeb/src/app/app.component.ts
+++ Front/ArchiWeb/src/app/app.component.ts
@@ -2,15 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConnexionComponent, HttpClientModule],
+  imports: [RouterOutlet, ConnexionComponent, HttpClientModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ArchiWeb';
   Name: string = "Milo Pauchet"
+
+  constructor(private router: Router) { }
+
+  Deco(): void {
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("role");
+    this.router.navigate(['']);
+  }
 }
