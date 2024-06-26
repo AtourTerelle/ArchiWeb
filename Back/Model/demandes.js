ede7dc823c939632bc14d3c95d9f187d173b670e
Doryan <122481253+AtourTerelle@users.noreply.github.com>
2024-06-26 12:47:40 +0200

    Clean

--- Back/Model/demandes.js
+++ Back/Model/demandes.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const demandeSchema = new mongoose.Schema({
-    //id_d: { type: Number, required: true, unique: true },
     user_pseudo: { type: String, required: true },
     materiel_nom: { type: String, required: true },
     type_d: { type: String, required: true },
     etats_d: { type: String, required: true, default: "EnAttente" },
-    salle_d: { type: String /*default: "default"*/}
+    salle_d: { type: String }
 });
 
 module.exports = mongoose.model('demandes', demandeSchema);
